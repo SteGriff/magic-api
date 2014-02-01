b368d33795636a8aff2a35962419540676791b63
Stephen Griffiths <github@stegriff.co.uk>
2014-02-01 10:09:59 +0000

    Improved Scry interface

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 
 function db(){
 	//Connect to db using MySQLi
-	$sv = "db425111171.db.1and1.com";
-	$un = "dbo425111171";
-	$conn = new mysqli($sv, $un, "24orangesegastap", "db425111171");
+	$sv = "server.address.com";
+	$un = "db-username";
+	$conn = new mysqli($sv, $un, "db-password", "db-name");
 	if ($conn->connect_error) die("Could not connect to database: " . $conn->connect_error);
 	return $conn;
 }
